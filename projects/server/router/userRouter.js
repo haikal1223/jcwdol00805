bf0f254230dc9679a46842c949576126d1b2a3d1
RaihanAdity <112265109+RaihanAdity@users.noreply.github.com>
2023-03-15 19:06:14 +0700

    [GP5-2] As a non-registered user, I want to be able to register as a user (#2)
    * [GP5-2] Register and Send Email
    
    * [GP5-2] Update
    
    Update userController.js-RegisterUser
    
    * [GP5-2] Revised Code
    
    Clearing code
    
    * [GP5-2]Updated 14/03/2023
    
    Updated Code

---
+++ projects/server/router/userRouter.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const Router = express.Router()
+
+//Import All Controller
+const { userController } = require('../controller')
+
+//Import JWTVerify
+
+Router.post('/register', userController.registerUser);
+// Router.get('/verification', userController.getData);
+// Router.patch('/verification/:uid', userController.inputPassword);
+// Router.post('/forgot-password', userController.forgotPassword)
+// Router.patch('/reset-password/:uid', userController.resetPassword)
+
+
+module.exports = Router
