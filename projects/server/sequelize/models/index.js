486d5c475aaaaf88c6b723636f6f4a48234bd9bc
RaihanAdity <112265109+RaihanAdity@users.noreply.github.com>
2023-03-16 22:04:45 +0700

    [GP5-2-FIX] Removes ignore on config

--- projects/server/sequelize/models/index.js
+++ projects/server/sequelize/models/index.js
@@ -6,21 +6,25 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js').db[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-const JSONConfig = JSON.parse(JSON.stringify(config))
 
 let sequelize;
-if (JSONConfig.use_env_variable) {
-    sequelize = new Sequelize(process.env[JSONConfig.use_env_variable], JSONConfig);
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(JSONConfig.database, JSONConfig.username, JSONConfig.password, JSONConfig);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
     .readdirSync(__dirname)
     .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js' &&
+            file.indexOf('.test.js') === -1
+        );
     })
     .forEach(file => {
         const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
