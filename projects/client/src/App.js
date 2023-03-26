fde87c50673d37691a4f6ff7d5884aba3cca3c6e
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-03-26 11:04:41 +0700

    [GP5-12] Finishing add product to cart

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -10,9 +10,9 @@ import Order from "./pages/Order";
 import Product from "./pages/Product";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-import Pages from './components/pages';
-import Activation from './pages/activation'
-import RegisterUser from './pages/Register';
+import Pages from "./components/pages";
+import Activation from "./pages/activation";
+import RegisterUser from "./pages/Register";
 // import ForgotPassword from './pages/forgotPassword';
 // import UpdatePassword from './pages/newPassword';
 
@@ -21,46 +21,43 @@ function App() {
 
   const keepLoggedIn = () => {
     try {
-      const token = localStorage.getItem('myToken')
+      const token = localStorage.getItem("myToken");
       if (token) {
-        setIsLoggedIn(true)
+        setIsLoggedIn(true);
       } else {
-        setIsLoggedIn(false)
+        setIsLoggedIn(false);
       }
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  }
+  };
 
   useEffect(() => {
-    keepLoggedIn()
-  }, [])
+    keepLoggedIn();
+  }, []);
 
   const RequireAuth = ({ children }) => {
-    const userIsLogged = localStorage.getItem('myToken')
+    const userIsLogged = localStorage.getItem("myToken");
 
     if (!userIsLogged) {
-      return (
-        <Navigate to='/' />
-      )
+      return <Navigate to="/" />;
     }
-    return children
-  }
+    return children;
+  };
 
   return (
     <div className="flex justify-center">
-
       <div className="w-[480px] z-0">
         <Navbar login={isLoggedIn} />
         <Routes>
-          <Route path='/' element={<Home login={isLoggedIn} />} />
-          <Route path='/activation' element={<Activation />} />
-          <Route path='/register' element={<RegisterUser />} />
-          <Route path='/product/:id' element={<Product login={isLoggedIn}/>} />
+          <Route path="/" element={<Home login={isLoggedIn} />} />
+          <Route path="/activation" element={<Activation />} />
+          <Route path="/register" element={<RegisterUser />} />
+          <Route path="/product/:id" element={<Product login={isLoggedIn} />} />
           <Route
             path="/cart"
             element={
-              <RequireAuth> 
+              <RequireAuth>
                 <Cart login={isLoggedIn} />
               </RequireAuth>
             }
@@ -80,7 +77,7 @@ function App() {
         <Toaster />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
