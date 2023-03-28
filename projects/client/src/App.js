27ff7270c10a67bf1a9f06228f35b1bcf75055e4
RaihanAdity <112265109+RaihanAdity@users.noreply.github.com>
2023-03-28 19:44:52 +0700

    [GP5-16] Order - As a user, I want to select, add a new or change the address on the checkout page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -14,6 +14,7 @@ import RegisterUser from "./pages/Register";
 import ForgotPassword from "./pages/forgotPassword";
 import UpdatePassword from "./pages/newPassword";
 import EditProfile from "./pages/editProfile";
+import CheckOut from "./pages/CheckOut";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -54,6 +55,7 @@ function App() {
           <Route path="/register" element={<RegisterUser />} />
           <Route path="/edit-profile" element={<EditProfile />} />
           <Route path="/product/:id" element={<Product login={isLoggedIn} />} />
+          <Route path="/checkout" element={<CheckOut />} />
           <Route
             path="/cart"
             element={
