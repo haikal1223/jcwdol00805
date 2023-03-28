a5133c42834960549299508463a4cbda538acffc
Dennis Destian <116083568+doomsdoge@users.noreply.github.com>
2023-03-28 22:01:17 +0700

    [GP5-19] Admin - As an admin, I want to be able to login to the admin dashboard (#13)
    * [GP5-19] Admin Login
    
    Updated app routing with new routes to /admin path
    Added conditional rendering between buyer facing and admin facing components
    
    * [GP5-19] Added verification for admin login
    
    Added login feature on admin facing routes
    Added verification for admin role before accessing admin paths
    Added sidebar with path dependent states
    
    * Resolve conflict app.js
    
    * Update Toast on Login component

--- projects/server/index.js
+++ projects/server/index.js
@@ -26,10 +26,13 @@ app.use(express.static('public'));
 
 const { userRouter } = require("./router");
 const { productRouter } = require('./router')
+const { adminRouter } = require('./router')
 const { cartRouter } = require('./router')
+app.use('/cart', cartRouter)
 app.use("/user", userRouter);
 app.use('/product', productRouter)
-app.use('/cart', cartRouter)
+app.use('/admin', adminRouter)
+
 
 // app.get("/api", (req, res) => {
 //   res.send(`Hello, this is my API`);
