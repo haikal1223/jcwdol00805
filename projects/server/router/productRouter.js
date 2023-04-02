524838be2a4d7800c6d1891cbfce156ac8e893af
RaihanAdity <112265109+RaihanAdity@users.noreply.github.com>
2023-04-02 22:37:07 +0700

    GP5-13 - Update Code

--- projects/server/router/productRouter.js
+++ projects/server/router/productRouter.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const Router = express.Router()
+const express = require("express");
+const Router = express.Router();
 
 // import controller
-const { productController } = require('../controller')
+const { productController } = require("../controller");
 
 // import middleware
 /* const decodeToken = require('./../middleware/decodeToken')
 const uploadImages = require('../middleware/upload') */
 
 // Router method
-Router.get('/view', productController.viewProduct)
+Router.get("/view", productController.viewProduct);
+Router.get("/productData", productController.viewProductData);
+Router.get("/detail", productController.viewDetailProduct);
+Router.get("/productStock", productController.viewProductStock);
 
-module.exports = Router
+module.exports = Router;
