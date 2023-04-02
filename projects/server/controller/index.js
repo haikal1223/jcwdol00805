e3a7cfbfd028ac8a420e40d2d3dd56643d99f117
RaihanAdity <112265109+RaihanAdity@users.noreply.github.com>
2023-04-02 22:37:15 +0700

    DONE

--- projects/server/controller/index.js
+++ projects/server/controller/index.js
@@ -2,10 +2,12 @@ const cartController = require("./cartController");
 const productController = require("./productController");
 const userController = require("./userController");
 const courierController = require("./courierController");
+const adminController = require("./adminController");
 
 module.exports = {
   cartController,
   productController,
   userController,
   courierController,
+  adminController,
 };
--- projects/server/router/index.js
+++ projects/server/router/index.js
@@ -2,10 +2,12 @@ const cartRouter = require("./cartRouter");
 const productRouter = require("./productRouter");
 const userRouter = require("./userRouter");
 const courierRouter = require("./courierRouter");
+const adminRouter = require("./adminRouter");
 
 module.exports = {
   cartRouter,
   productRouter,
   userRouter,
   courierRouter,
+  adminRouter,
 };
