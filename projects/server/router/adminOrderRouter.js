75384f2c5c7124d48ca0688af92f9d253c84cc41
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-04-25 22:14:16 +0700

    Admin can cancel the order
    - Admin can cancel the order if the order status is not Shipped, Delivered, or Cancelled
    - After that, order status is changed to Cancelled

--- projects/server/router/adminOrderRouter.js
+++ projects/server/router/adminOrderRouter.js
@@ -6,6 +6,7 @@ const { adminOrderController } = require('../controller')
 
 Router.get('/view/:wh_id', adminOrderController.viewOrder)
 Router.get('/order-detail/:id', adminOrderController.orderDetail)
+Router.get('/cancel-order/:id', adminOrderController.cancelOrder)
 
 
 module.exports = Router
