30a017cf84028db5b5be3580f3e59f944862476a
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-05-15 16:22:55 +0700

    Update code

--- projects/client/src/components/cartCard.jsx
+++ projects/client/src/components/cartCard.jsx
@@ -26,6 +26,9 @@ export default function CartCard(props) {
   useEffect(() => {
     fetchProduct();
   }, []);
+  useEffect(() => {
+    fetchProduct();
+  }, [props.deleteFunction]);
 
   return (
     <>
